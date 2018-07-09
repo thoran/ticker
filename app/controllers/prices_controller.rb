e701be3ac42699bae651ede29e193f0e163261a1
thoran <code@thoran.com>
2018-07-09 10:29:16 +1000

    Able to show prices for each pair.

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,11 +1,34 @@
-class PricesController
+class PricesController < ApplicationController
 
-  def index
-    
+  def show
+    @prices = Price.where(pair: show_prices_params).limit(10)
   end
 
-  def prices_params
-    params.require(:pair)
+  def create
+    @prices = JSON.parse(create_prices_params)
+    respond_to do |format|
+      if Price.transaction{Price.create(@prices)}
+        format.html{redirect_to :root, notice: 'Prices successfully inserted.' }
+        format.json{render :show, status: :created, location: @prices}
+      else
+        format.html{render :new}
+        format.json{render json: @prices.errors, status: :unprocessable_entity}
+      end
+    end
   end
+
+  private
+
+  def show_prices_params
+    params.require(:id)
+  end
+
+  def create_prices_params
+    params.require(:prices)
+  end
+
+  # def permitted_fields
+  #   %i{pair volume_24 volume transition_time status session prev_close last current_time bid ask}
+  # end
 
 end
