fcf37af6d2b5a1c60eef92a0538f56ed4b108a12
thoran <code@thoran.com>
2018-07-11 23:20:00 +1000

    Custom permitted fields code for JSON blob.

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,13 +1,12 @@
 class PricesController < ApplicationController
 
   def show
-    @prices = Price.where(pair: show_prices_params).limit(10)
+    @prices = Price.where(pair: show_prices_params)
   end
 
   def create
-    @prices = JSON.parse(create_prices_params)
     respond_to do |format|
-      if Price.transaction{Price.create(@prices)}
+      if Price.transaction{Price.create(create_prices_params)}
         format.html{redirect_to :root, notice: 'Prices successfully inserted.' }
         format.json{render :show, status: :created, location: @prices}
       else
@@ -23,12 +22,31 @@ class PricesController < ApplicationController
     params.require(:id)
   end
 
+  # def create_prices_params
+  #   params.require(:prices)
+  #   prices = JSON.parse(params[:prices])
+  #   prices.collect! do |price|
+  #     if price.keys.all?{|k| create_permitted_fields.include?(k)}
+  #       price
+  #     else
+  #       nil
+  #     end
+  #   end
+  #   raise ActionController::UnpermittedParameters if prices.any?{|e| e.nil?}
+  #   prices
+  # end
+
   def create_prices_params
     params.require(:prices)
+    prices = JSON.parse(params[:prices])
+    prices.collect! do |price|
+      price.slice(*create_permitted_fields)
+    end
+    prices
   end
 
-  # def permitted_fields
-  #   %i{pair volume_24 volume transition_time status session prev_close last current_time bid ask}
-  # end
+  def create_permitted_fields
+    @create_permitted_fields ||= %w{pair volume_24h volume transition_time status session prev_close last current_time bid ask}
+  end
 
 end
